c7f3e24921a55851454e42f33ead81d0f07e0e46
Mechatron00 <dixitdinesh52@gmail.com>
2023-01-29 18:38:13 +0530

    revised02

---
+++ script.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  console.log(document.body);
+  console.log(params);
+});
